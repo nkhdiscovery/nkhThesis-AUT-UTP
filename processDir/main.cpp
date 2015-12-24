fdaa857533e635f8df2537759316ba868e9e2fe5
nkh <navid.khazaee@gmail.com>
2015-12-24 16:01:13 +0330

    nkhTest

--- processDir/main.cpp
+++ processDir/main.cpp
@@ -65,6 +65,8 @@ string fpsCalcEnd()
         timerCounter = 0;
     return string(fpsString);
 }
+void nkhTest();
+
 
 int main(int argc, char *argv[])
 {
@@ -102,8 +104,14 @@ int main(int argc, char *argv[])
 
 void nkhMain(path inDir, path outDir, vector<path> frames)
 {
+    nkhTest();
+    return;
+
+
     initFPSTimer();
     int fpsSum = 0, counter = 0 ;
+
+
     for (vector<path>::const_iterator it(frames.begin()), it_end(frames.end()); it != it_end; ++it)
     {
         fpsCalcStart();
@@ -115,7 +123,7 @@ void nkhMain(path inDir, path outDir, vector<path> frames)
         cv::threshold(src, dst, 128.0, 255.0, cv::THRESH_BINARY);
         //cv::imshow("Result", dst);
         */
-
+        /*
         cv::Mat src_host = cv::imread(it->string(), cv::IMREAD_GRAYSCALE), dst_host;
         cv::cuda::GpuMat dst, src;
         src.upload(src_host);
@@ -123,7 +131,7 @@ void nkhMain(path inDir, path outDir, vector<path> frames)
         cv::cuda::threshold(dst, src, 128.0, 255.0, cv::THRESH_BINARY);
         cv::Mat result_host(src);
         //cv::imshow("Result", result_host);
-
+        */
 
         /*
         Mat img = imread(it->string());
@@ -147,4 +155,141 @@ void nkhMain(path inDir, path outDir, vector<path> frames)
         counter++;
     }
     cout << (double) fpsSum/counter << endl;
+
+}
+
+void nkhTest()
+{
+    long fpsSum = 0, counter = 0 ;
+    int iLowH = 68;
+    int iHighH = 93;
+
+    int iLowS = 40;
+    int iHighS = 255;
+
+    int iLowV = 47;
+    int iHighV = 174;
+
+    int iLowB = 68;
+    int iHighB = 93;
+
+    int iLowG = 63;
+    int iHighG = 255;
+
+    int iLowR = 55;
+    int iHighR = 174;
+
+    int hueValue = 255;
+
+    Mat src, dst, imgHSV;
+    Mat dst2 ;
+    VideoCapture cap("../test/35.mp4");
+    initFPSTimer();
+    while(true)
+    {
+        fpsCalcStart();
+        cap>>src ;
+        if( src.size().area() <=0 )
+        {
+            break;
+        }
+        resize(src,src, cvSize(src.size().width/2,src.size().height/2));
+        cvtColor(src, imgHSV, COLOR_BGR2HSV);
+        inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), dst); //Threshold the image
+        //        inRange(src, Scalar(iLowB, iLowG, iLowR), Scalar(iHighB, iHighG, iHighR), dst2); //Threshold the image
+
+
+        //morphological opening (remove small objects from the foreground)
+        erode(dst, dst, getStructuringElement(MORPH_ELLIPSE, Size(5,5)) );
+        dilate( dst, dst, getStructuringElement(MORPH_ELLIPSE, Size(5,5)) );
+        //morphological closing (fill small holes in the foreground)
+        dilate( dst, dst, getStructuringElement(MORPH_ELLIPSE, Size(5,5)) );
+        erode(dst, dst, getStructuringElement(MORPH_ELLIPSE, Size(5,5)) );
+
+        //        Mat resized;
+        //resize(dst,dst, cvSize(dst.size().width/2.0,dst.size().height/2.0));
+
+
+
+        medianBlur(dst,dst,5);
+
+        //nkhImshow_Write(src, dst, "HSV", "HSV","green");
+        //        nkhImshow_Write(src, dst2, "RGB", "RGB","green");
+
+        vector<vector<Point> > contours;
+        vector<Vec4i> order;
+        GaussianBlur( dst, dst, Size(5, 5), 1,1 );
+        Mat nullImg ;
+        int params[]={70,210,3};
+        Mat edges;
+        blur( dst, edges, Size(3,3) );
+        Canny( dst, edges, params[0], params[1],params[2]);
+        findContours( edges, contours, order, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+        Mat showContours = Mat::zeros( edges.size(), CV_8UC3 );
+        resize(src,showContours,cvSize(src.size().width/2.0,src.size().height/2.0));
+
+
+
+        RNG rng(12345);
+
+        vector<vector<Point> > contoursApprox;
+        contoursApprox.resize(contours.size());
+        for( size_t k = 0; k < contours.size(); k++ )
+        {
+            double epsilon = 0.05*arcLength(contours[k],true);
+            approxPolyDP(Mat(contours[k]), contoursApprox[k], epsilon, true);
+        }
+
+        vector<Moments> mu(contoursApprox.size() );
+        for( int i = 0; i < contoursApprox.size(); i++ )
+        { mu[i] = moments( contoursApprox[i], false ); }
+
+
+        vector<Point2f> mc( contoursApprox.size() );
+        for( int i = 0; i < contoursApprox.size(); i++ )
+        { mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); }
+
+        vector<RotatedRect> minRect( contours.size() );
+        for( int i = 0; i< contoursApprox.size(); i++ )
+        {
+            long long carea = contourArea(contoursApprox[i]) ;
+            long long area = dst.size().width*dst.size().height;
+            minRect[i] = minAreaRect( Mat(contoursApprox[i]) );
+            if( carea< (area)/900.0 || carea > (area)/3.0
+                    || (int)mc[i].y > dst.size().height/2  || minRect[i].size.width < minRect[i].size.height
+                    || (minRect[i].angle <-30 ))
+                continue;
+
+            Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+            //            drawContours( showContours, contoursApprox, i, color, 2, 8, order, 0, Point() );
+            Point2f rect_points[4]; minRect[i].points( rect_points );
+            for( int j = 0; j < 4; j++ )
+                line( showContours, rect_points[j], rect_points[(j+1)%4], color, 1, 8 );
+        }
+        imshow("Contoures", showContours);
+
+
+
+        //        for( int i = 0; i< contours.size(); i++ )
+        //        {
+        //            Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+        //            drawContours( showContours, contours, i, color, 2, 8, order, 0, Point() );
+        //        }
+        //        imshow("Contoures",showContours);
+        imshow("Thresh",dst);
+
+
+        fpsCalcEnd();
+        if(timerFPS !=INFINITY )
+        {
+            fpsSum += timerFPS;
+            //cout << counter << " " << timerFPS << endl ;
+        }
+        counter++;
+        cout << timerFPS << endl;
+        if (waitKey(10) == 'q') //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+        {
+            break;
+        }
+    }
 }
