a67f16cf2e1c3adb05a47911bec078b4d786e24c
nkh <navid.khazaee@gmail.com>
2016-01-07 00:06:44 +0330

    Huge Commit, huge failure today. Gonna for FFT match my Idea

--- processDir/main.cpp
+++ processDir/main.cpp
@@ -54,7 +54,7 @@ using namespace boost::accumulators;
 #define DOMAIN_SIGMA_S 30 //30 orig// 20 green & brown
 #define DOMAIN_SIGMA_R 270 //350 orig //190 in green //290 brown
 #define DOMAIN_MAX_ITER 3
-#define DTF_METHOD "IC"
+#define DTF_METHOD "RC"
 
 /*----------------- nkhEnd: global vars and defs -----------------*/
 
@@ -506,6 +506,54 @@ void connectedMask(cv::Mat& smoothed, cv::Mat& conMap)
 //    devMap.download(conMap);
 }
 
+void getDFTPlanes(cv::Mat& I, cv::Mat& magI)
+{
+    cv::Mat padded;                            //expand input image to optimal size
+    int m = cv::getOptimalDFTSize( I.rows );
+    int n = cv::getOptimalDFTSize( I.cols ); // on the border add zero values
+    cv::copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, cv::BORDER_CONSTANT, cv::Scalar::all(0));
+
+    cv::Mat planes[] = {cv::Mat_<float>(padded), cv::Mat::zeros(padded.size(), CV_32F)};
+    cv::Mat complexI;
+    cv::merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
+
+    cv::dft(complexI, complexI);            // this way the result may fit in the source matrix
+
+    // compute the magnitude and switch to logarithmic scale
+    // => log(1 + sqrt(Re(DFT(I))^2 + Im(DFT(I))^2))
+    cv::split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+    cv::magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
+    magI = planes[0];
+
+    magI += cv::Scalar::all(1);                    // switch to logarithmic scale
+    cv::log(magI, magI);
+
+    // crop the spectrum, if it has an odd number of rows or columns
+    magI = magI(cv::Rect(0, 0, magI.cols & -2, magI.rows & -2));
+
+    // rearrange the quadrants of Fourier image  so that the origin is at the image center
+    int cx = magI.cols/2;
+    int cy = magI.rows/2;
+
+    cv::Mat q0(magI, cv::Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
+    cv::Mat q1(magI, cv::Rect(cx, 0, cx, cy));  // Top-Right
+    cv::Mat q2(magI, cv::Rect(0, cy, cx, cy));  // Bottom-Left
+    cv::Mat q3(magI, cv::Rect(cx, cy, cx, cy)); // Bottom-Right
+
+    cv::Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
+    q0.copyTo(tmp);
+    q3.copyTo(q0);
+    tmp.copyTo(q3);
+
+    q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
+    q2.copyTo(q1);
+    tmp.copyTo(q2);
+
+    cv::normalize(magI, magI, 0, 1, CV_MINMAX); // Transform the matrix with float values into a
+                                            // viewable image form (float between values 0 and 1).
+
+}
+
 /*------------------- nkhEnd Saliency -------------------*/
 
 void evaluateMasked(cv::Mat& masked, map<int, FrameObjects>& groundTruth, int frameNum, vector<double>& result);
@@ -553,6 +601,7 @@ int main(int argc, char *argv[])
     //return a.exec();
 }
 
+
 void nkhMain(path inVid, path inFile, path outDir)
 {
     //Open the video file
@@ -590,6 +639,11 @@ void nkhMain(path inVid, path inFile, path outDir)
     on_trackbarR( sigmaR, 0 );
     */
 
+    /*
+    cv::Mat template1 = cv::imread("1.png");
+    cv::cuda::GpuMat devTemplate(template1);
+    */
+
     while(true)
     {
         cap >> currentFrame;
@@ -600,9 +654,11 @@ void nkhMain(path inVid, path inFile, path outDir)
         }
         fpsCalcStart();
         
-        cv::Mat frameResized, frameResized_gray;
+        cv::Mat frameResized, frameResized_gray, frameResizedHalf;
         cv::resize(currentFrame, frameResized, cv::Size(currentFrame.size().width/RESIZE_FACTOR,
                                              currentFrame.size().height/RESIZE_FACTOR));
+        cv::resize(currentFrame, frameResizedHalf, cv::Size(currentFrame.size().width/2,
+                                             currentFrame.size().height/2));
         cv::cvtColor(frameResized, frameResized_gray, cv::COLOR_BGR2GRAY);
 
         //TODO: implement with GPU
@@ -611,7 +667,7 @@ void nkhMain(path inVid, path inFile, path outDir)
         dtfWrapper(frameResized, edgeSmooth);
 
         cv::Mat edgeSmoothLow;
-        cv::ximgproc::dtFilter(edgeSmooth, frameResized, edgeSmoothLow, 25, 60, cv::ximgproc::DTF_NC); //r 350. 50. nc
+        cv::ximgproc::dtFilter(edgeSmooth, frameResized, edgeSmoothLow, 15, 30, cv::ximgproc::DTF_NC); //r 350. 50. nc
 
 
         //SaliencyMap
@@ -653,20 +709,27 @@ void nkhMain(path inVid, path inFile, path outDir)
         */
 
 
-        int erosionDilation_size = 7;
+        int erosionDilation_size = 3;
         cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT,
                                                     cv::Size(2*erosionDilation_size + 1,
                                                              2*erosionDilation_size+1));
 
         cv::Mat colorMask = greenMask | whiteMask | brownMask ;
         cv::blur(colorMask, colorMask, cv::Size(12,12));
+        cv::erode(colorMask, colorMask, element);
+        cv::dilate(colorMask, colorMask, element);
+
+        edgeSmoothLow.copyTo(masked, colorMask);
+        cv::Mat twoThird = cv::Mat::zeros(edgeSmooth.rows, edgeSmooth.cols, edgeSmooth.type());
+        twoThird(cv::Rect(0,0, twoThird.size().width, 2*twoThird.size().height/3.0)) = cv::Scalar::all(255);
+        masked.copyTo(twoThird, twoThird);
+        computeSaliency(twoThird, 32, saliency);
+        saliency *= 3;
+        saliency.convertTo( saliency, CV_8U );
+        threshold( saliency, binMask, 0, 255, cv::THRESH_BINARY );
         fin = colorMask & binMask;
-        cv::dilate(fin, fin, element);
-        cv::erode(fin, fin, element);
-
-
-        edgeSmooth.copyTo(masked, fin);
-
+        masked.release();
+        twoThird.copyTo(masked, fin);
 
         /*
          * GPU
@@ -711,8 +774,24 @@ void nkhMain(path inVid, path inFile, path outDir)
 
         //Visualize
 
-        char controlChar = maybeImshow("Final", masked  ) ;
-        //controlChar = maybeImshow("Smooth", edgeSmooth);
+        /*
+        cv::Mat frameResizedHalf_gray;
+        cv::cvtColor(frameResizedHalf, frameResizedHalf_gray, cv::COLOR_BGR2GRAY);
+        cv::cuda::GpuMat devFrame(frameResizedHalf_gray), devResult;
+        cv::Ptr<cv::cuda::TemplateMatching> matcherPtr = cv::cuda::createTemplateMatching(frameResizedHalf_gray.type(), CV_TM_CCORR);
+        matcherPtr->match(devFrame, devTemplate, devResult);
+
+        double maxVal;
+        cv::Point location;
+        cv::cuda::minMaxLoc(devResult, 0, &maxVal, 0, &location);
+        if(maxVal > 0 )
+        {
+            cout << maxVal << endl ;
+        }
+        */
+
+        char controlChar = maybeImshow("Final", frameResized) ;
+//        controlChar = maybeImshow("Gray",frameResizedHalf );
         if (controlChar == 'q')
         {
             break;
@@ -725,7 +804,6 @@ void nkhMain(path inVid, path inFile, path outDir)
         {
             cap.set(CV_CAP_PROP_POS_AVI_RATIO , 0);
         }
-
 
 
         fpsCalcEnd();
